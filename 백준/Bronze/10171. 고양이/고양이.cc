dbe3ca49f25837cb94730e95b2508e26380dcfe6
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2024-02-06 00:25:13 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// \, ', " 출력할 경우 앞에 \ 붙이기
+
+int main() {
+	cout << "\\    /\\" << endl;
+	cout << " )  ( \')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+
+	return 0;
+}
