82e43eb61e6c2174c24b1969a8437690e10a2672
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2024-02-13 23:26:37 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);	
+	cin.tie(NULL);	
+	
+	string s;
+	
+	cin >> s;
+
+	cout << s.length();
+}
