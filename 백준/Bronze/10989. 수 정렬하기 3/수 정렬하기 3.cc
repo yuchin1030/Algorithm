4e0dad1db93d3c195ea7843ef1c14f1482978a8c
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2024-11-29 17:21:08 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1540 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	int N;
 
 	cin >> N;
@@ -25,3 +28,27 @@ int main()
 		}
 	}
 }
+/*
+메모리 용량 : 8MB
+int 배열로 하면 4byte * 천만 = 40000KB = 40MB
+(1byte * 1024 = 1KB, 1KB * 1024 = 1MB, 1MB * 1024 = 1GB)
+
+즉, 배열에 수를 저장하여 정렬하는 아래 방법은 적합하지 않음
+int N;
+
+cin >> N;
+
+vector<int> arr(N);
+
+for (int i = 0; i < N; i++)
+{
+	cin >> arr[i];
+}
+
+stable_sort(arr.begin(), arr.end());
+
+for (int i = 0; i < N; i++)
+{
+	cout << arr[i] << "\n";
+}
+*/
