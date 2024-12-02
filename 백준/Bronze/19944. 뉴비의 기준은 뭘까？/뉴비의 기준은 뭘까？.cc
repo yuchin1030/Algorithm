e7386566fe76d9245bafd0c409fa2244be8cea77
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2024-12-02 16:59:23 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, M;
+
+	cin >> N >> M;
+
+	if (M == 1 || M == 2)
+		cout << "NEWBIE!";
+	else if (M <= N)
+		cout << "OLDBIE!";
+	else
+		cout << "TLE!";
+}
