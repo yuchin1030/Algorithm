a95e4cf6597899f9df8753618d1bc1429f2397ef
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2024-12-05 19:19:41 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -3,61 +3,58 @@
 #include <string>
 using namespace std;
 
-int main() {
-	stack<int> s;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-	int N;	// 명령의 수
-	int num; // push할 때 스택에 넣을 정수
-	int value;	// 출력되는 결과값
-	string command;	// 명령어
+	int N;
+	stack<int> stack;
 
-	cin >> N;	// 명령할 명령어의 개수 입력
+	cin >> N;
 
-	// 주어진 명령어의 수만큼 반복
-	for (int i = 0; i < N; i++) {
-		
-		cin >> command;	// 명령할 함수 입력
-		
-		if (command == "push") {
-			cin >> num;		// 스택에 넣을 숫자 입력
-			s.push(num);	// 스택에 넣음
+	for (int i = 0; i < N; i++)
+	{
+		string str;
+		cin >> str;
+
+		if (str == "push")
+		{
+			string num;
+			cin >> num;
+
+			stack.push(stoi(num));
 		}
-		else if (command == "pop") {
-			if (s.size() == 0) {	// 스택이 비어있다면
-				value = -1;	
-				cout << value << endl;	// -1 출력
+		else if (str == "pop")
+		{
+			if (stack.empty())
+			{
+				cout << "-1\n";
 			}
-			else {
-				value = s.top();	// 결과값에 가장 위에 있는 정수 먼저 저장
-				s.pop();	// 스택의 가장 위에 있는 정수를 빼고
-				cout << value << endl;	// 그 수를 출력
-			}
-		}
-		else if (command == "size") {
-			cout << s.size() << endl;	// 스택에 들어있는 정수의 개수 출력
-		}
-		else if (command == "empty") {	
-			if (s.size() == 0) {	// 스택이 비어있다면 1 출력
-				value = 1;		
-				cout << value << endl;
-			}
-			else {		// 아니면 0 출력
-				value = 0;
-				cout << value << endl;
+			else
+			{
+				cout << stack.top() << "\n";
+				stack.pop();
 			}
 		}
-		else if (command == "top") {
-			if (s.size() == 0) {	// 스택이 비어있다면 -1 출력
-				value = -1;
-				cout << value << endl;	
+		else if (str == "size")
+		{
+			cout << stack.size() << "\n";
+		}
+		else if (str == "empty")
+		{
+			cout << stack.empty() << "\n";
+		}
+		else if (str == "top")
+		{
+			if (stack.empty())
+			{
+				cout << "-1\n";
 			}
-			else {
-				value = s.top();	// 아니면 가장 위에 있는 정수 출력
-				cout << value << endl;
+			else
+			{
+				cout << stack.top() << "\n";
 			}
-
 		}
 	}
-
-	return 0;
 }
