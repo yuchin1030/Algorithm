efa8bd55eb646c736e22d1b058a52deda2d0c238
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2024-12-20 16:39:34 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int N;
+	cin >> N;
+	
+	queue<int> q;
+	
+	for (int i = 0; i < N; i++)
+	{
+		string str;
+		cin >> str;
+		
+		if (str == "push")
+		{
+			int num;
+			cin >> num;
+			
+			q.push(num);
+		}
+		else if (str == "pop")
+		{
+			if (!q.empty())
+			{
+				cout << q.front() << "\n";
+				q.pop();
+			}
+			else
+			{
+				cout << -1 << "\n";
+			}
+		}
+		else if (str == "size")
+		{
+			cout << q.size() << "\n";
+		}
+		else if (str == "empty")
+		{
+			cout << q.empty() << "\n";
+		}
+		else if (str == "front")
+		{
+			if (!q.empty())
+			{
+				cout << q.front() << "\n";
+			}
+			else
+			{
+				cout << -1 << "\n";
+			}
+		}
+		else if (str == "back")
+		{
+			if (!q.empty())
+			{
+				cout << q.back() << "\n";
+			}
+			else
+			{
+				cout << -1 << "\n";
+			}
+		}
+	}
+}
