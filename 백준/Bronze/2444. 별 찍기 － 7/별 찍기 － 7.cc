1974b28fc99ab7a40803ea456532785d3975e53a
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2025-02-20 14:17:27 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N - i - 1; j++)
+		{
+			cout << " ";
+		}
+		
+		for (int k = 0; k < 2 * i + 1; k++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < N - 1; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << " ";
+		}
+		
+		for (int k = 0; k < 2 * (N - i) - 3; k++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
