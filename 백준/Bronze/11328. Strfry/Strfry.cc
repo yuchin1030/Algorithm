968d641eae0613a2ce72e5b7ca47198210db0e75
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2025-02-20 23:51:09 +0900

    [Bronze II] Title: Strfry, Time: 56 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		bool bPossible = true;
+
+		string str1, str2;
+		cin >> str1 >> str2;
+
+		vector<int> v(26, 0);
+
+		if (str1.length() == str2.length())
+		{
+			for (int j = 0; j < str1.length(); j++)
+			{
+				v[str1[j] - 'a']++;
+				v[str2[j] - 'a']--;
+			}
+
+			for (int k = 0; k < 26; k++)
+			{
+				if (v[k] != 0)
+				{
+					bPossible = false;
+					break;
+				}
+			}
+
+		}
+		else
+		{
+			bPossible = false;
+		}
+
+		if (bPossible)
+			cout << "Possible\n";
+		else
+			cout << "Impossible\n";
+	}
+}
