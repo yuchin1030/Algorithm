c85135b162a52742dbe5bd5cf3a356b65dabbb9c
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2025-03-09 21:46:12 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+
+	queue<int> q;
+
+	for (int i = 1; i <= N; i++)
+	{
+		q.push(i);
+	}
+
+	cout << "<";
+	while (q.size() != 1)
+	{
+		for (int i = 0; i < K - 1; i++)
+		{
+			int temp = q.front();
+			q.pop();
+			q.push(temp);
+		}
+
+		cout << q.front() << ", ";
+		q.pop();
+	}
+
+	cout << q.front() << ">";
+}
