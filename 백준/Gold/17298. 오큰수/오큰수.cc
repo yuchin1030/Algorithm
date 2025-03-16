34bb5e1e81b54b4eb9d77ac02268a0d8aa78d5a2
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2025-03-16 20:50:43 +0900

    [Gold IV] Title: 오큰수, Time: 184 ms, Memory: 12376 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	stack<int> s;
+	int num[1000001];
+	int result[1000001];
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num[i];
+	}
+
+	for (int i = N - 1; i >= 0; i--)
+	{
+		while (!s.empty() && num[i] >= s.top())
+			s.pop();
+
+		if (s.empty())
+			result[i] = -1;
+		else
+			result[i] = s.top();
+
+		s.push(num[i]);
+	}
+
+	for (int i = 0; i < N; i++)
+		cout << result[i] << " ";
+}
+// 참고 - https://cocoon1787.tistory.com/347
