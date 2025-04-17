fe9b2665cc75e7297606d2b04d5e028459943e17
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2025-04-17 13:41:39 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int Rev(string str)
+{
+	reverse(str.begin(), str.end());
+
+	int num = stoi(str);
+
+	return num;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string X, Y;
+	cin >> X >> Y;
+
+	string temp = to_string(Rev(X) + Rev(Y));
+
+	stack<char> st;
+
+	for (int i = 0; i < temp.length(); i++)
+	{
+		st.push(temp[i]);
+	}
+	
+	bool flag = false;
+
+	while (!st.empty())
+	{
+		if (st.top() == '0' && !flag)
+		{
+			st.pop();
+			continue;
+		}
+		else
+		{
+			//cout << "?";
+			flag = true;
+			cout << st.top();
+			st.pop();
+		}
+		
+	}
+	
+}
