6d3d3ff7f6f79e4aca90ca778cd984ab9b935a35
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2025-04-30 18:18:46 +0900

    [Bronze I] Title: 유레카 이론, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.cc
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.cc
@@ -10,17 +10,17 @@ int main()
 	int T;
 	cin >> T;
 
+	vector<int> v(45);
+	for (int i = 1; i <= 44; i++)
+	{
+		v[i] = i * (i + 1) / 2;
+	}
+
 	// n(n+1) / 2 <= 1000 --> n <= 44
 	for (int i = 0; i < T; i++)
 	{
 		int num;
 		cin >> num;
-
-		vector<int> v(45);
-		for (int j = 1; j <= 44; j++)
-		{
-			v[j] = j * (j + 1) / 2;
-		}
 
 		bool flag = false;
 		for (int m = 1; m <= 44; m++)
