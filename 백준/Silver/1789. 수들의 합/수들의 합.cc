ca86a03c4706191c5870a444b5afc3255e6844a9
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2025-05-15 12:47:04 +0900

    [Silver V] Title: 수들의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long S;
+	cin >> S;
+
+	long long sum = 0, num = 1, cnt = 0;
+
+	while (true)
+	{
+		sum += num;
+		cnt++;
+
+		if (sum > S)
+		{
+			cnt--;
+			break;
+		}
+
+		num++;
+	}
+
+	cout << cnt;
+}
