014d96b932d3e27162ab572f858a97ca79ee5ef8
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2025-06-02 16:38:30 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,26 +1,21 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-int main() 
+int main()
 {
 	int L; string str;
+	long long r = 1; 
+	long long M = 1234567891;
+	long long sum = 0;
 	
 	cin >> L >> str;
-	
-	int num[L]; int sum = 0;
-	
-	// 1. 문자열 숫자로 바꾸기
+
 	for (int i = 0; i < L; i++)
 	{
-		num[i] = str[i] - 'a' + 1;
+		sum += ((str[i] - 'a' + 1) * r) % M;
+		sum %= M;
+		r = (r * 31) % M;
 	}
-	
-	// 2. 해시함수 수식 구현
-	for (int j = 0; j < L; j++)
-	{
-		sum += num[j] * pow(31, j);
-	}
-	
+
 	cout << sum;
 }
