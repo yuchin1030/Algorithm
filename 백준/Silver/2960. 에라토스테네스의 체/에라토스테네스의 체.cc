ae6e3d30b93ba98c1942ba9cc2cc29b644cab45e
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2025-06-24 17:09:22 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, K;
+	cin >> N >> K;
+
+	map<int, bool> m;
+
+	for (int i = 1; i <= N; i++)
+	{
+		m.insert({i, false});
+	}
+
+	int cnt = 0; int index = 2;
+	for (int i = 2; i <= N; i++)
+	{
+		for (int j = i; j <= N; j += i)
+		{
+			if (cnt == K)
+				break;
+			else if (m[j])
+				continue;
+			else
+			{
+				m[j] = true;
+				cnt++;
+				index = j;
+			}
+		}
+		
+	}
+
+	cout << index;
+}
