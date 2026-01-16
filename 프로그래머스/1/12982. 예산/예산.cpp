5995cd48c3664cdba40feb77acb4a34a14ede23e
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2026-01-16 17:38:44 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    
+    sort(d.begin(), d.end());
+    
+    int sum = 0;
+    for (int i = 0; i < d.size(); i++)
+    {
+        sum += d[i];
+        
+        if (sum > budget)
+            break;
+        
+        answer++;
+    }
+    
+    return answer;
+}
