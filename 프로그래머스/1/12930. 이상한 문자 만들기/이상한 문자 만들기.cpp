7d02c0c73d4796297b030a2f0596e8a12d21c25a
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2026-01-20 00:26:50 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    int index = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ' ')
+        {
+            index = -1;
+        }
+        else
+        {
+            if (index % 2 == 0)
+            {
+                s[i] = toupper(s[i]);
+            }
+            else
+            {
+                s[i] = tolower(s[i]);
+            }
+        }
+        
+        index++;
+    }
+    
+    return s;
+}
