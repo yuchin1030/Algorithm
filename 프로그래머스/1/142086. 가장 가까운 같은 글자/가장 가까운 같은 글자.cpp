569437886eacad44348e676a3808769eb78631c0
yuchin1030 <109638387+yuchin1030@users.noreply.github.com>
2026-01-20 16:15:47 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.30 ms, Memory: 5.27 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    
+    for (int i = 0; i < s.length(); i++)
+    {
+        bool flag = false;
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (s[i] == s[j])
+            {
+                answer.push_back(i - j);
+                flag = true;
+                break;
+            }
+        }
+        
+        if (!flag)
+            answer.push_back(-1);
+    }
+    
+    return answer;
+}
